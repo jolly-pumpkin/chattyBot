47288abb03c60e9a1f695433ab4afbc53909001e
cneill <cneill@indeed.com>
2025-02-26 21:32:23 -0500

    wips

--- chat/chat/tokens.py
+++ chat/chat/tokens.py
@@ -4,3 +4,44 @@ tokenizer = tiktoken.get_encoding("cl100k_base")
 count = len(tokenizer.encode("Answer the question based on the context"))
 
 print(count)
+
+
+def create_prompt(question, df, max_token_count):
+    """
+    Given a question and a dataframe containing rows of text and their
+    embeddings, return a text prompt to send to a Completion model
+    """
+    # Create a tokenizer that is designed to align with our embeddings
+    tokenizer = tiktoken.get_encoding("cl100k_base")
+
+    # Count the number of tokens in the prompt template and question
+    prompt_template = """
+Answer the question based on the context below, and if the question
+can't be answered based on the context, say "I don't know"
+
+Context: 
+
+{}
+
+---
+
+Question: {}
+Answer:"""
+
+    current_token_count = len(tokenizer.encode(prompt_template)) + \
+                            len(tokenizer.encode(question))
+
+    context = []
+    for text in get_rows_sorted_by_relevance(question, df)["text"].values:
+
+        # Increase the counter based on the number of tokens in this row
+        text_token_count = len(tokenizer.encode(text))
+        current_token_count += text_token_count
+
+        # Add the row of text to the list if we haven't exceeded the max
+        if current_token_count <= max_token_count:
+            context.append(text)
+        else:
+            break
+
+    return prompt_template.format("\n\n###\n\n".join(context), question)
